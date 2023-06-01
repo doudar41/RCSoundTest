09cc97cc65c920a7f397629327598503eca0eeee
Evgeni Doudar <60257081+doudar41@users.noreply.github.com>
2023-06-01 20:23:08 +0200

    Update RCRevEngineFunction.cs

--- RCRevEngineFunction.cs
+++ RCRevEngineFunction.cs
@@ -1,25 +1,75 @@
+[SerializeField]
+private AnimationCurve rpmRise;
+[SerializeField]
+private AnimationCurve speedCheckCurve;
+[SerializeField]
+private AnimationCurve[] gearChangeRPMCurves;
+[SerializeField]
+private StudioEventEmitter engineFMOD;
+[SerializeField]
+private StudioEventEmitter brakesFMOD;
+
+[SerializeField]
+private float rpmCoefficient;
+
+public int currentGear;
+float RPM;
+
+//Changes to Audio Control function
+public void AudioControl()
+{
+
+	//audios
+	if (IsGrounded)
+	{
+		if (Mathf.Abs(carVelocity.x) > SkidEnable - 0.1f)
+		{
+			if (ControlType == ControlType.PLAYER)
+			{
+				brakesFMOD.SetParameter("BrakeBool", 1);
+				if (!brakesFMOD.IsPlaying()) brakesFMOD.Play();
+			}
+		}
+		else
+		{
+			brakesFMOD.SetParameter("BrakeBool", 0);
+		}
+	}
+	else
+	{
+		brakesFMOD.SetParameter("BrakeBool", 0);
+	}
+
+}
+		
+
+
+
 void GearChangeEmulator()
         {
-			if (currentSpeed != 0)
+	
+	// Gear switching check if speed / maxSpeed is more than value of a point on animation curve
+		if (currentSpeed != 0)
+		{
+			float speedCheckpoint = currentSpeed / maxSpeed;
+			var points = speedCheckCurve.keys;
+			for (int i = points.Length-1; i > 0; i--)
 			{
-				float speedCheckpoint = currentSpeed / maxSpeed;
-				var points = speedCheckCurve.keys;
-				for (int i = points.Length-1; i > 0; i--)
+				if (points[i].value < speedCheckpoint)
 				{
-					if (points[i].value < speedCheckpoint)
-					{
-						if (currentGear != i)
-                        {
-							currentGear = i;
-							gearText.text = currentGear.ToString();
-							pressAccelarateTime = 0;
-							engineFMOD.SetParameter("Boost", 0);
-						}
-						break;
+					if (currentGear != i)
+		{
+						currentGear = i;
+						pressAccelarateTime = 0;
+						engineFMOD.SetParameter("Boost", 0);
 					}
+					break;
 				}
 			}
+		}
 			
+	//Toggle count of pressing accelerate button
+	
 			if (Input.GetKeyDown(KeyCode.UpArrow))
             {
 				fakeBoost = true;
@@ -29,40 +79,39 @@ void GearChangeEmulator()
 				fakeBoost = false;
 			}
 
+	// Count time of pressing accelerate button
+	
 			if (fakeBoost && IsGrounded)
-            {
+            		{
 				pressAccelarateTime += Time.deltaTime*0.2f;
-				//textEngine.text = gearChangeRPMCurves[currentGear].Evaluate(pressAccelarateTime).ToString();
-				textEngine.text = currentSpeed.ToString();
 				RPM = gearChangeRPMCurves[currentGear].Evaluate(pressAccelarateTime) * rpmCoefficient;
-
-
+				//Randomize RPM
 				engineFMOD.SetParameter("RPM", Random.Range(RPM - 10,RPM + 10));
+				
+				//additional pitch
 				if(pressAccelarateTime != 0)
-                {
+                		{
 				var pitchShift = rpmRise.Evaluate(1/pressAccelarateTime);
-					engineFMOD.SetParameter("Boost", Random.Range(pitchShift - 10,pitchShift + 10) );
-                }
-
-
+				engineFMOD.SetParameter("Boost", Random.Range(pitchShift - 10,pitchShift + 10) );
+               			 }
 			}
 			else
-            {
-				pressAccelarateTime = 0;
-				engineFMOD.SetParameter("RPM", currentSpeed);
-				engineFMOD.SetParameter("Boost", 0);
-				textEngine.text = pressAccelarateTime.ToString();
-				if (currentSpeed <= 1)
-                {
-					currentGear = 0;
+			{
+			//slowly return to idle
+			pressAccelarateTime = 0;
+			engineFMOD.SetParameter("RPM", currentSpeed);
+			engineFMOD.SetParameter("Boost", 0);
+			if (currentSpeed <= 1)
+				{
+				currentGear = 0;
 				}
 			}
 		} 
-
+		
+		// Example of mix changing function
 		public void EngineVolumeChange(float engineVol)
-        {
+   		{
 			EngineBus.setVolume(engineVol);
-
 		}
 
 		public void BrakesVolumeChange(float brakesVol)
